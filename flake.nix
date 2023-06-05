0c5a56f1dd22152ee67801adcba0a706825cdbff
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2023-06-05 12:34:51 +0200

    chore: update dev env
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,38 +3,45 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
 
-        versions = pkgs.writeScriptBin "versions" ''
-          kind --version
-          kubectl version --short --client
-          echo helm $(helm version --short)
-          vault --version
-        '';
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = [ versions ] ++ (with pkgs; [
-            versions
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devenv.shells.default = {
+          packages = with pkgs; [
             kind
             kubectl
             kustomize
             kubernetes-helm
 
             vault
-          ]);
+          ];
 
-          shellHook = ''
+          scripts = {
+            versions.exec = ''
+              kind --version
+              kubectl version --short --client
+              echo helm $(helm version --short)
+              vault --version
+            '';
+          };
+
+          enterShell = ''
             versions
           '';
+
+          # https://github.com/cachix/devenv/issues/528#issuecomment-1556108767
+          containers = pkgs.lib.mkForce { };
         };
-      }
-    );
+      };
+    };
 }
